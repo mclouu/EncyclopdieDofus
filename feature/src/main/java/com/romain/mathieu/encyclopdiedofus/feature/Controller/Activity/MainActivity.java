e9b39731859b01f4c4ff9237f3500d053e35a50d
romain <rmathieu13@gmail.com>
2018-12-18 17:05:16 +0100

    implements the fragments (pages) at drawer menu and makes them functional

--- feature/src/main/java/com/romain/mathieu/encyclopdiedofus/feature/Controller/Activity/MainActivity.java
+++ feature/src/main/java/com/romain/mathieu/encyclopdiedofus/feature/Controller/Activity/MainActivity.java
@@ -1,10 +1,10 @@
 package com.romain.mathieu.encyclopdiedofus.feature.Controller.Activity;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,13 +12,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.romain.mathieu.encyclopdiedofus.feature.Controller.Fragment.BestiaireFragment;
+import com.romain.mathieu.encyclopdiedofus.feature.Controller.Fragment.ConsommableFragment;
+import com.romain.mathieu.encyclopdiedofus.feature.Controller.Fragment.CosmetiqueFragment;
+import com.romain.mathieu.encyclopdiedofus.feature.Controller.Fragment.EquipementFragment;
+import com.romain.mathieu.encyclopdiedofus.feature.Controller.Fragment.RessourceFragment;
 import com.romain.mathieu.encyclopdiedofus.feature.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Fragment fragmentRessource;
+    private Fragment fragmentConsommable;
+    private Fragment fragmentEquipement;
+    private Fragment fragmentBestiaire;
+    private Fragment fragmentCosmetique;
+
+    public static final int FRAGMENT_EQUIPEMENT = 0;
+    public static final int FRAGMENT_CONSOMMABLE = 1;
+    public static final int FRAGMENT_RESSOURCE = 2;
+    public static final int FRAGMENT_COSMETIQUE = 3;
+    public static final int FRAGMENT_BESTIAIRE = 4;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -29,7 +48,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "TDB !! mega TDB !!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -81,25 +100,100 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.equipement) {
-            // Handle the camera action
-        } else if (id == R.id.consommables) {
-
-        } else if (id == R.id.ressources) {
-
-        } else if (id == R.id.cosmetiques) {
-
-        } else if (id == R.id.bestiaire) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        // 6 - Show fragment after user clicked on a menu item
+        switch (id) {
+            case R.id.equipement:
+                this.showFragment(FRAGMENT_EQUIPEMENT);
+                break;
+            case R.id.consommables:
+                this.showFragment(FRAGMENT_CONSOMMABLE);
+                break;
+            case R.id.ressources:
+                this.showFragment(FRAGMENT_RESSOURCE);
+                break;
+            case R.id.cosmetiques:
+                this.showFragment(FRAGMENT_COSMETIQUE);
+                break;
+            case R.id.bestiaire:
+                this.showFragment(FRAGMENT_BESTIAIRE);
+                break;
+            default:
+                break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // ---------------------
+    // FRAGMENTS
+    // ---------------------
+
+    // 5 - Show fragment according an Identifier
+
+    private void showFragment(int fragmentIdentifier) {
+        switch (fragmentIdentifier) {
+            case FRAGMENT_EQUIPEMENT:
+                this.showEquipementFragment();
+                break;
+            case FRAGMENT_CONSOMMABLE:
+                this.showconsommableFragment();
+                break;
+            case FRAGMENT_RESSOURCE:
+                this.showRessourceFragment();
+                break;
+            case FRAGMENT_COSMETIQUE:
+                this.showcosmetiqueFragment();
+                break;
+            case FRAGMENT_BESTIAIRE:
+                this.showbestiaireFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // 4 - Create each fragment page and show it
+
+    private void showEquipementFragment() {
+        if (this.fragmentEquipement == null)
+            this.fragmentEquipement = EquipementFragment.newInstance();
+        this.startTransactionFragment(this.fragmentEquipement);
+    }
+
+    private void showconsommableFragment() {
+        if (this.fragmentConsommable == null)
+            this.fragmentConsommable = ConsommableFragment.newInstance();
+        this.startTransactionFragment(this.fragmentConsommable);
+    }
+
+    private void showRessourceFragment() {
+        if (this.fragmentRessource == null)
+            this.fragmentRessource = RessourceFragment.newInstance();
+        this.startTransactionFragment(this.fragmentRessource);
+    }
+
+    private void showcosmetiqueFragment() {
+        if (this.fragmentCosmetique == null)
+            this.fragmentCosmetique = CosmetiqueFragment.newInstance();
+        this.startTransactionFragment(this.fragmentCosmetique);
+    }
+
+    private void showbestiaireFragment() {
+        if (this.fragmentBestiaire == null)
+            this.fragmentBestiaire = BestiaireFragment.newInstance();
+        this.startTransactionFragment(this.fragmentBestiaire);
+    }
+
+    // 3 - Generic method that will replace and show a fragment inside the MainActivity Frame Layout
+
+    private void startTransactionFragment(Fragment fragment) {
+        if (!fragment.isVisible()) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.activity_main_frame_layout, fragment).commit();
+        }
+    }
+
+
 }
