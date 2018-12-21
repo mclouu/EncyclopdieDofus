8a47bd321096b7ca3d688f252aa9e7b3a009f5ff
romain <rmathieu13@gmail.com>
2018-12-21 02:03:57 +0100

    Create stream (Observable) for request (RxJava)

---
+++ feature/src/main/java/com/romain/mathieu/encyclopdiedofus/feature/Model/DofusStream.java
@@ -0,0 +1,22 @@
+package com.romain.mathieu.encyclopdiedofus.feature.Model;
+
+import com.romain.mathieu.encyclopdiedofus.feature.Model.API.EquipementDofus.EquipementDofus;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class DofusStream {
+
+    public static Observable<EquipementDofus> streamFetchItem() {
+        DofusService dofusService = DofusService.retrofit.create(DofusService.class);
+        return dofusService.getItemDofus()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .timeout(10, TimeUnit.SECONDS);
+    }
+
+
+}
