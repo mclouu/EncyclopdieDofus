ca21f2c74dfea5b04dd875875c75d6c47d490d7c
romain <rmathieu13@gmail.com>
2019-01-04 17:39:04 +0100

    add library stetho

--- feature/src/main/java/com/romain/mathieu/encyclopdiedofus/feature/Model/DofusService.java
+++ feature/src/main/java/com/romain/mathieu/encyclopdiedofus/feature/Model/DofusService.java
@@ -1,8 +1,10 @@
 package com.romain.mathieu.encyclopdiedofus.feature.Model;
 
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.romain.mathieu.encyclopdiedofus.feature.Model.API.EquipementDofus.EquipementDofus;
 
 import io.reactivex.Observable;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -12,8 +14,13 @@ public interface DofusService {
 
     MyConstant constant = new MyConstant();
 
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .addNetworkInterceptor(new StethoInterceptor())
+            .build();
+
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(constant.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build();
